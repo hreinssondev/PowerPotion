d59b61b3ba3cd104094bb2de6e4428d7159ab319
hreinssondev <hreinssondev@gmail.com>
2026-06-08 21:10:54 +0200

    Initial Commit

---
+++ CoolAeid/CoolAeidApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolAeidApp.swift
+//  CoolAeid
+//
+//  Created by H on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoolAeidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
